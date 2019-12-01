a3f7768e2d88f0d002d36682e51422078e0cb0bb
Xynnn_ <278954852@qq.com>
2019-12-01 20:33:42 +0800

    Add files via upload
    version 0.1
    -可以在Linux（Ubuntu16.04）下运行
    -运行环境go、protobuf

---
+++ Worker.go
@@ -0,0 +1,273 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"./pb"
+
+	"github.com/golang/protobuf/proto"
+
+	"flag"
+	"time"
+)
+
+//处理用户请求
+func HandleConn(conn net.Conn) {
+	//函数调用完毕，自动关闭conn
+	defer conn.Close()
+
+	//获取客户端的网络地址信息
+	addr := conn.RemoteAddr().String()
+	fmt.Println(addr, " conncet sucessful")
+
+	data := make([]byte, 512)
+	res:= &pb.Message{}
+	for {
+		//读取包
+		_, err := conn.Read(data)
+		if err != nil {
+			fmt.Println("err = ", err)
+			return
+		}
+		//解包
+		message := &pb.Message{
+		}
+		err = proto.Unmarshal(data,message)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		seq := message.Seq
+		//构造包
+		res.Seq = seq
+
+		//开始判断字段
+		switch message.MsgType {
+		case pb.Message_HEARTBEAT_REQ:
+			//接收到心跳检测包，则向master回报现在是在线的
+
+			//产生反馈的数据
+
+			res.MsgType = pb.Message_HEARTBEAT_RES
+			//把数据转换成字节流
+			data,err = proto.Marshal(res)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			conn.Write([]byte(data))
+			return
+		case pb.Message_CALCULATE_REQ:
+			goto calculate
+		default:
+			return
+		}
+
+
+calculate:
+	//计算
+		//构造一个回馈的包
+
+		res.MsgType = pb.Message_CALCULATE_RES
+		CalresMessage := &pb.CalcRes{
+			Status:pb.CalcRes_OK,
+		}
+		//把计算请求解包
+		Calcreq := message.GetCalcreq()
+		switch Calcreq.Type {
+		case pb.CalculateTypes_INTEGER32:
+			var addAns, minAns, mulAns, divAns int32
+			addAns = Calcreq.Int32Op1 + Calcreq.Int32Op2
+			minAns = Calcreq.Int32Op1 - Calcreq.Int32Op2
+			mulAns = Calcreq.Int32Op1 * Calcreq.Int32Op2
+			if Calcreq.Int32Op2 == 0{
+				divAns = 0
+				//除0
+				res.Calcres.Status = pb.CalcRes_ERROR
+			}else{
+				divAns = Calcreq.Int32Op1 / Calcreq.Int32Op2
+			}
+
+			//判断溢出，涉及到四种运算，其他的先不管了
+			overflow := (Calcreq.Int32Op1 < 0 && Calcreq.Int32Op2 < 0 && addAns > 0) || (Calcreq.Int32Op1 > 0 && Calcreq.Int32Op2 > 0 && addAns < 0)
+
+			if overflow {
+				CalresMessage.Status = pb.CalcRes_ERROR
+			}
+			int32ans:= &pb.Int32Ans{
+				addAns,
+				minAns,
+				mulAns,
+				 divAns,
+			}
+
+			CalresMessage.Type = pb.CalculateTypes_INTEGER32
+			CalresMessage.Int32Ans = int32ans
+
+		case pb.CalculateTypes_INTEGER64:
+			var addAns, minAns, mulAns, divAns int64
+			addAns = Calcreq.Int64Op1 + Calcreq.Int64Op2
+			minAns = Calcreq.Int64Op1 - Calcreq.Int64Op2
+			mulAns = Calcreq.Int64Op1 * Calcreq.Int64Op2
+			if Calcreq.Int64Op2 == 0{
+				divAns = 0
+				//除0
+				CalresMessage.Status = pb.CalcRes_ERROR
+			}else{
+				divAns = Calcreq.Int64Op1 / Calcreq.Int64Op2
+			}
+
+			//判断溢出，涉及到四种运算，其他的先不管了
+			overflow := (Calcreq.Int64Op1 < 0 && Calcreq.Int64Op2 < 0 && addAns > 0) || (Calcreq.Int64Op1 > 0 && Calcreq.Int64Op2 > 0 && addAns < 0)
+
+			if overflow {
+				CalresMessage.Status = pb.CalcRes_ERROR
+			}
+
+			int64ans:= &pb.Int64Ans{
+				addAns,
+				minAns,
+				mulAns,
+				divAns,
+			}
+			CalresMessage.Type = pb.CalculateTypes_INTEGER64
+			CalresMessage.Int64Ans = int64ans
+
+		case pb.CalculateTypes_FLOAT32:
+			var addAns, minAns, mulAns, divAns float32
+			addAns = Calcreq.Float32Op1 + Calcreq.Float32Op2
+			minAns = Calcreq.Float32Op1 - Calcreq.Float32Op2
+			mulAns = Calcreq.Float32Op1 * Calcreq.Float32Op2
+			if (Calcreq.Float32Op2 - 0) < 1e-6{
+				divAns = 0
+				//除0
+				CalresMessage.Status = pb.CalcRes_ERROR
+			}else{
+				divAns = Calcreq.Float32Op1 / Calcreq.Float32Op2
+			}
+
+			//判断溢出，涉及到四种运算，其他的先不管了
+			var nan bool = false
+			if mulAns != mulAns || divAns != divAns {
+				nan = true
+			}
+
+			if nan {
+				CalresMessage.Status = pb.CalcRes_ERROR
+			}
+			float32ans:= &pb.Float32Ans{
+				addAns,
+				minAns,
+				mulAns,
+				divAns,
+			}
+
+			CalresMessage.Float32Ans = float32ans
+			CalresMessage.Type = pb.CalculateTypes_FLOAT32
+
+		case pb.CalculateTypes_FLOAT64:
+			var addAns, minAns, mulAns, divAns float64
+			addAns = Calcreq.Float64Op1 + Calcreq.Float64Op2
+			minAns = Calcreq.Float64Op1 - Calcreq.Float64Op2
+			mulAns = Calcreq.Float64Op1 * Calcreq.Float64Op2
+			if (Calcreq.Float64Op2 - 0) < 1e-12{
+				divAns = 0
+				//除0
+				CalresMessage.Status = pb.CalcRes_ERROR
+			}else{
+				divAns = Calcreq.Float64Op1 / Calcreq.Float64Op2
+			}
+
+			//判断溢出，涉及到四种运算，其他的先不管了
+			var nan bool = false
+			if mulAns != mulAns || divAns != divAns {
+				nan = true
+			}
+
+			if nan {
+				CalresMessage.Status = pb.CalcRes_ERROR
+			}
+
+			float64ans:= &pb.Float64Ans{
+				addAns,
+				minAns,
+				mulAns,
+				divAns,
+			}
+
+			CalresMessage.Float64Ans = float64ans
+			CalresMessage.Type = pb.CalculateTypes_FLOAT64
+		}
+
+		res.Calcres = CalresMessage
+		//把数据转换成字节流
+		data,err = proto.Marshal(res)
+		if err != nil {
+			fmt.Println(err)
+		}
+		conn.Write([]byte(data))
+		return
+
+	}
+
+}
+
+func main() {
+	//从输入参数得到master地址
+	var masterIP		string
+	//调用形式 xxx -h masterhost:port
+	flag.StringVar(&masterIP, "h",  "","master的socket地址，默认为空")
+	flag.Parse()
+
+	if "" == masterIP {
+		println("No master socket.")
+		println("Please use parameters:-h masterhost:port")
+		return
+	}
+	//构造一个包
+	registReq := &pb.Message{
+		MsgType:pb.Message_REGISTER_REQ,
+		Seq: int32(time.Now().Unix()),
+		Socket:"3743",
+		//注意这里默认就把端口设置成3743了，如果需要可以添加随机产生一个端口的方式
+	}
+	registReqData,err := proto.Marshal(registReq)
+	if err != nil {
+		fmt.Println(err)
+	}
+	//与master建立连接
+	conn, err := net.Dial("tcp", masterIP)
+	if err != nil {
+		fmt.Println(err)
+	}
+	//完了要关闭
+	fmt.Println("Connect Master Successfully!")
+	defer conn.Close()
+	//把包发过去
+	conn.Write([]byte(registReqData))
+
+	//事实上不用接到master的反馈也行，虽然定义了
+
+	fmt.Println("Starting Listening ::3743...")
+	//监听
+	listener, err := net.Listen("tcp", "127.0.0.1:3743")
+	if err != nil {
+		fmt.Println("err = ", err)
+		return
+	}
+
+	defer listener.Close()
+
+	//接收多个用户
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("err = ", err)
+			return
+		}
+
+		//处理用户请求, 新建一个协程
+		go HandleConn(conn)
+	}
+
+}
