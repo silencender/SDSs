0689ed9dc2a1365d884b7a52e275ddba4917a4c8
liquor233 <hongyibuju15@sjtu.edu.cn>
2019-12-08 09:49:15 +0800

    auto-shell

--- cmd/client-driver/main.go
+++ cmd/client-driver/main.go
@@ -7,9 +7,5 @@ import (
 
 func main() {
     log.Println("client running")
-    client := client.StartClient()
-    if client.Master.Socket == nil {
-        return
-    }
-    client.Run("i","1","2")
+    client.NewClient(1) 
 }
